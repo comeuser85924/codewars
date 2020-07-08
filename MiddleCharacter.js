d3ef66d953f1aede5c6a8869716f1859b196dc5a
comeuser85924 <zx2515296964@gmail.com>
2020-07-08 21:54:46 +0800

    fixed

--- MiddleCharacter.js
+++ MiddleCharacter.js
@@ -13,7 +13,7 @@ function getMiddle(s)
   return s.slice(s.length/2,(s.length/2)+1)
 }
 
-/* Clever Answer
+/* Clever Answer 
 function getMiddle(s)
 {
   return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
