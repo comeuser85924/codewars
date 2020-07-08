0517575041c4dbd7bd0a28542f14727f400df68b
comeuser85924 <zx2515296964@gmail.com>
2020-07-08 21:55:22 +0800

    fixed-2

--- MiddleCharacter.js
+++ MiddleCharacter.js
@@ -16,6 +16,6 @@ function getMiddle(s)
 /* Clever Answer 
 function getMiddle(s)
 {
-  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);   
 }
 */
