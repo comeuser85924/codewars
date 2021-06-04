12a871ce9f4fd6c983a82736fc90e175905943a0
comuser85924 <zx2515296964@gmail.com>
2021-06-05 03:22:37 +0800

    Decode the Morse code

---
+++ javascript/6kuy/Decode the Morse code.js
@@ -0,0 +1,14 @@
+/* Decode the Morse code [6 kyu]
+    https://www.codewars.com/kata/54b724efac3d5402db00065e
+*/
+decodeMorse = function(morseCode){
+    function decodeMorseLetter(letter) {
+      return MORSE_CODE[letter];
+    }
+    function decodeMorseWord(word) {
+      return word.split(' ').map(decodeMorseLetter).join('');
+    }
+    return morseCode.trim().split('   ').map(decodeMorseWord).join(' ');
+  }
+
+decodeMorse('HEY JUDE')
