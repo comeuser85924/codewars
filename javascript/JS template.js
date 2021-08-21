54eb827dae6231991b41e1c91ea8f7f3276326a5
comuser85924 <zx2515296964@gmail.com>
2021-08-21 17:47:44 +0800

    add JS template file

---
+++ javascript/JS template.js
@@ -0,0 +1,17 @@
+/* My Train Title [{Title Rank} kyu]
+  Title description...
+*/
+
+
+//My Answer
+function abc(a,b){
+ //your code here
+}
+
+//Test
+convertFrac('2,4,2,6,2,8')
+convertFrac('6,12,4,12,3,12')
+
+/* Solution Clever Answer
+
+*/
